f83bc415cbbd95b47fc025c94066d2585ce9f860
Vishwas <vishwaskumar445@gmail.com>
2023-06-18 19:47:02 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_notification/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(_firebaseBackgroundMessageHandler);
+  runApp(const MyApp());
+}
+
+@pragma('vm:entry-point')
+Future<void> _firebaseBackgroundMessageHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print(message.notification!.title.toString());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen());
+  }
+}
---
+++ lib/notification_services.dart
@@ -0,0 +1,164 @@
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_notification/message_screen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationServices {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  void initLocalNotification(
+      BuildContext context, RemoteMessage message) async {
+    var androidInitializationSettings =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iosInitializationSettings = const DarwinInitializationSettings();
+
+    var initializationSettings = InitializationSettings(
+      android: androidInitializationSettings,
+      iOS: iosInitializationSettings,
+    );
+    await _flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (payload) {
+        handleMessageNotification(context, message);
+      },
+    );
+  }
+
+  // void firebaseInit(BuildContext context) async {
+  //   FirebaseMessaging.onMessage.listen((message) {
+  //     if (kDebugMode) {
+  //       print(message.notification!.title.toString());
+  //       print(message.notification!.body.toString());
+  //       print(message.data.toString());
+  //     }
+  //     if (Platform.isIOS) {
+  //       forgroundMessage();
+  //     }
+
+  //     if (Platform.isAndroid) {
+  //       initLocalNotification(context, message);
+  //       showNotification(message);
+  //     } else {
+  //       showNotification(message);
+  //     }
+  //   });
+  // }
+
+  // Future<void> showNotification(RemoteMessage message) async {
+  //   AndroidNotificationChannel channel = AndroidNotificationChannel(
+  //     message.notification!.android!.channelId.toString(),
+  //     message.notification!.android!.channelId.toString(),
+  //     importance: Importance.max,
+  //     showBadge: true,
+  //     playSound: true,
+  //   );
+
+  //   AndroidNotificationDetails androidNotificationDetails =
+  //       AndroidNotificationDetails(
+  //     channel.id.toString(),
+  //     channel.name.toString(),
+  //     channelDescription: 'your channel description',
+  //     icon: "@mipmap/ic_launcher",
+  //     importance: Importance.high,
+  //     priority: Priority.high,
+  //     playSound: true,
+  //     ticker: 'ticker',
+  //   );
+
+  //   const DarwinNotificationDetails darwinNotificationDetails =
+  //       DarwinNotificationDetails(
+  //           presentAlert: true, presentBadge: true, presentSound: true);
+
+  //   NotificationDetails notificationDetails = NotificationDetails(
+  //       android: androidNotificationDetails, iOS: darwinNotificationDetails);
+
+  //   Future.delayed(Duration.zero, () {
+  //     _flutterLocalNotificationsPlugin.show(
+  //       0,
+  //       message.notification!.title.toString(),
+  //       message.notification!.body.toString(),
+  //       notificationDetails,
+  //     );
+  //   });
+  // }
+
+  // Future<void> requestNotificationPermission() async {
+  //   print("Entering request permission...");
+  //   NotificationSettings settings = await messaging.requestPermission(
+  //     alert: true,
+  //     announcement: true,
+  //     badge: true,
+  //     carPlay: true,
+  //     criticalAlert: true,
+  //     provisional: true,
+  //     sound: true,
+  //   );
+  //   print("Exiting request permission............");
+  //   if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+  //     if (kDebugMode) {
+  //       print('User granted permission');
+  //     }
+  //   } else if (settings.authorizationStatus ==
+  //       AuthorizationStatus.provisional) {
+  //     if (kDebugMode) {
+  //       print('User granted provisional permission');
+  //     }
+  //   } else {
+  //     if (kDebugMode) {
+  //       print('User declined or has not accepted permission');
+  //     }
+  //   }
+  // }
+
+  // Future<String> getDeviceToken() async {
+  //   String? token = await messaging.getToken();
+  //   return token!;
+  // }
+
+  // void isTokenRefresh() async {
+  //   messaging.onTokenRefresh.listen((event) {
+  //     print(event.toString());
+  //   });
+  // }
+
+  // Future<void> setupInteractMessage(BuildContext context) async {
+  //   //When app is terminated........................
+  //   RemoteMessage? initialMessage =
+  //       await FirebaseMessaging.instance.getInitialMessage();
+
+  //   if (initialMessage != null) {
+  //     handleMessageNotification(context, initialMessage);
+  //   }
+
+  //   //When app is killed............................
+  //   FirebaseMessaging.onMessageOpenedApp.listen((event) {
+  //     handleMessageNotification(context, event);
+  //   });
+  // }
+
+  // void handleMessageNotification(BuildContext context, RemoteMessage message) {
+  //   if (message.data['type'] == 'msg') {
+  //     Navigator.push(
+  //         context,
+  //         MaterialPageRoute(
+  //             builder: (context) => MessageScreen(
+  //                   id: message.data['id'],
+  //                 )));
+  //   }
+  // }
+
+  // Future forgroundMessage() async {
+  //   await FirebaseMessaging.instance
+  //       .setForegroundNotificationPresentationOptions(
+  //     alert: true,
+  //     sound: true,
+  //     badge: true,
+  //   );
+  // }
+}
