e2b305233ea1271f292f8b10debb9795347e39d7
Vishwas <vishwaskumar445@gmail.com>
2023-06-19 12:43:55 +0530

    showNotifications

--- lib/notification_services.dart
+++ lib/notification_services.dart
@@ -50,43 +50,43 @@ class NotificationServices {
   //   });
   // }
 
-  // Future<void> showNotification(RemoteMessage message) async {
-  //   AndroidNotificationChannel channel = AndroidNotificationChannel(
-  //     message.notification!.android!.channelId.toString(),
-  //     message.notification!.android!.channelId.toString(),
-  //     importance: Importance.max,
-  //     showBadge: true,
-  //     playSound: true,
-  //   );
+  Future<void> showNotification(RemoteMessage message) async {
+    AndroidNotificationChannel channel = AndroidNotificationChannel(
+      message.notification!.android!.channelId.toString(),
+      message.notification!.android!.channelId.toString(),
+      importance: Importance.max,
+      showBadge: true,
+      playSound: true,
+    );
 
-  //   AndroidNotificationDetails androidNotificationDetails =
-  //       AndroidNotificationDetails(
-  //     channel.id.toString(),
-  //     channel.name.toString(),
-  //     channelDescription: 'your channel description',
-  //     icon: "@mipmap/ic_launcher",
-  //     importance: Importance.high,
-  //     priority: Priority.high,
-  //     playSound: true,
-  //     ticker: 'ticker',
-  //   );
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      channel.id.toString(),
+      channel.name.toString(),
+      channelDescription: 'your channel description',
+      icon: "@mipmap/ic_launcher",
+      importance: Importance.high,
+      priority: Priority.high,
+      playSound: true,
+      ticker: 'ticker',
+    );
 
-  //   const DarwinNotificationDetails darwinNotificationDetails =
-  //       DarwinNotificationDetails(
-  //           presentAlert: true, presentBadge: true, presentSound: true);
+    const DarwinNotificationDetails darwinNotificationDetails =
+        DarwinNotificationDetails(
+            presentAlert: true, presentBadge: true, presentSound: true);
 
-  //   NotificationDetails notificationDetails = NotificationDetails(
-  //       android: androidNotificationDetails, iOS: darwinNotificationDetails);
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails, iOS: darwinNotificationDetails);
 
-  //   Future.delayed(Duration.zero, () {
-  //     _flutterLocalNotificationsPlugin.show(
-  //       0,
-  //       message.notification!.title.toString(),
-  //       message.notification!.body.toString(),
-  //       notificationDetails,
-  //     );
-  //   });
-  // }
+    Future.delayed(Duration.zero, () {
+      _flutterLocalNotificationsPlugin.show(
+        0,
+        message.notification!.title.toString(),
+        message.notification!.body.toString(),
+        notificationDetails,
+      );
+    });
+  }
 
   Future<void> requestNotificationPermission() async {
     print("Entering request permission...");
